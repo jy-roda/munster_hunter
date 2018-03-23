e62ae6fa2eafcc77de4594ef83b67b9d712febac
abdelkader.ziane-cherif@etu.univ-amu.fr <abdelkader.ziane-cherif@etu.univ-ame.fr>
2018-03-23 11:34:52 +0100

    random mmr

--- target/classes/com/company/Main.java
+++ target/classes/com/company/Main.java
@@ -3,12 +3,15 @@ package com.company;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+
+import static java.lang.Math.random;
 import static java.lang.Math.toIntExact;
 
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Main {
 
@@ -35,8 +38,10 @@ public class Main {
             JSONArray a = (JSONArray) parser.parse(r);
 
             for (Object o : a) {
+                int rand = (int) (Math.random() * ( 2000 - 100));
                 JSONObject player = (JSONObject) o;
                 Joueur j = new Joueur((String) player.get("playerID"),  toIntExact((Long)player.get("id")),0 );
+                j.setMmr(rand);
                 System.out.println(j.toString());
                 joueurs.add(j);
             }
